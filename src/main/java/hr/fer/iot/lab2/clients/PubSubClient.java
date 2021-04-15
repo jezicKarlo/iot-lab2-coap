8563393dfa26dc3b8fb57eec70fac904842436e3
kjezic <karlo.jezic@minus5.hr>
2021-04-15 17:49:16 +0200

    done

--- src/main/java/hr/fer/iot/lab2/clients/PubSubClient.java
+++ src/main/java/hr/fer/iot/lab2/clients/PubSubClient.java
@@ -2,17 +2,21 @@ package hr.fer.iot.lab2.clients;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapObserveRelation;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.elements.exception.ConnectorException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PubSubClient {
 
-    public static void main(String[] args) throws ConnectorException, IOException {
+    public static void main(String[] args) {
         CoapClient client = new CoapClient("coap://localhost/sensors/temp");
-        client.observeAndWait(new CoapHandler() {
+
+        CoapObserveRelation relation = client.observe(new CoapHandler() {
             @Override
             public void onLoad(CoapResponse coapResponse) {
                 if (coapResponse != null && coapResponse.getCode() == CoAP.ResponseCode.CONTENT) {
@@ -21,12 +25,16 @@ public class PubSubClient {
                     System.out.println("Request failed");
                 }
             }
-
-
             @Override
             public void onError() {
                 System.out.println("on error");
             }
         });
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            br.readLine();
+        } catch (IOException e) { }
+
+        relation.proactiveCancel();
     }
 }
